f3a9f96e727d04472746bff6d962d7afcee23f32
Harshitha G <harshithagharshithag35@gmail.com>
2026-02-27 10:02:25 +0530

    Fixed port for Render

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
 
+const PORT = process.env.PORT || 5000;
+
+
 const app = express();
 
 connectDB();
@@ -14,7 +17,6 @@ app.use("/api/issues", require("./routes/issueRoutes"));
 app.get("/", (req, res) => {
   res.send("CivicConnect Backend is Running");
 });
-
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
